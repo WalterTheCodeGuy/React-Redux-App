57e5bdc48e9b5288dd52d11f8d713ad6b4b421ac
Walter Futch <walter.futch@gmail.com>
2019-12-13 01:48:31 -0500

    Updated styling

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -4,23 +4,36 @@ import Loader from 'react-loader-spinner';
 
 import {getAdvice} from '../actions';
 
+let today = new Date().toLocaleDateString();
+
 const Advice = props => {
   return (
-    <div>
-      <h1>Random Life Advice</h1>
-        {!props.advice && !props.isFetching && <p>Get yourself some random life advice!</p>}
-        {props.isFetching && (
-          <Loader type="Puff" color='#00BFFF' height={100} width={100} />
-        )}
-        {props.advice && <p>{props.advice.advice}</p>}
-        <button onClick={props.getAdvice}>Get Some Advice</button>
+    <div className='wrapper'>
+      <div className='paper'>
+        <div className='padder'>
+          <h1>Random Advice Slip</h1>
+            {!props.advice && !props.isFetching && <p className='advice'>Get a random advice slip!</p>}
+            {props.isFetching && (
+              <Loader type="Puff" color='#00BFFF' height={100} width={100} />
+            )}
+            {props.advice && 
+              <div className='slip'>
+                <p className='advice'>{props.advice.advice}</p> 
+                <p className='datetime'><span className='time'>#{props.advice.slip_id}</span> <span className='date'>{today}</span></p>
+              </div>
+            }
+        </div>     
+      </div>
+      <button onClick={props.getAdvice}>Get Some Advice</button>
     </div>
+    
   );
 };
 
 const mapStateToProps = state => {
   return {
     advice: state.advice,
+    slip_id: state.slip_id,
     isFetching: state.isFetching,
     error: state.error
   };
